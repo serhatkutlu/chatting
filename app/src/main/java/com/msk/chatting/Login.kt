98fef690379aa55c19424fbec6e9c6c14304faef
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2021-12-30 18:38:01 +0300

    Login page and Signup page created

--- app/src/main/java/com/msk/chatting/Login.kt
+++ app/src/main/java/com/msk/chatting/Login.kt
@@ -1,11 +1,53 @@
 package com.msk.chatting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
+    private val mAuth=FirebaseAuth.getInstance()
+    private lateinit var email:EditText
+    private lateinit var password:EditText
+    private lateinit var button_login:Button
+    private lateinit var button_signup:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        email=login_mail
+        password=login_password
+        button_login=login_loginButton
+        button_signup=login_signupButton
+
+
+
+        button_signup.setOnClickListener{
+            var intent=Intent(this,SignUp::class.java)
+            startActivity(intent)
+        }
+        button_login.setOnClickListener {
+           var email=email.text.toString()
+            var password=password.text.toString()
+            login(email,password)
+        }
+    }
+    fun login(email:String,password:String ){
+mAuth.signInWithEmailAndPassword(email,password)
+    .addOnCompleteListener{
+        if (it.isSuccessful){
+            val intent=Intent(this,MainActivity::class.java)
+        }
+        else{
+            Toast.makeText(this,"exeption",Toast.LENGTH_LONG).show()
+        }
+
+
+}
     }
 }
--- app/src/main/java/com/msk/chatting/SignUp.kt
+++ app/src/main/java/com/msk/chatting/SignUp.kt
@@ -1,11 +1,57 @@
 package com.msk.chatting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUp : AppCompatActivity() {
+    private  var mAuth=FirebaseAuth.getInstance()
+    private lateinit var name: EditText
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var button_login: Button
+    private lateinit var button_signup: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        name=Signup_name
+        email=Signup_mail
+        password=Signup_password
+        button_login=signup_loginButton
+        button_signup=signup_signupButton
+
+        button_signup.setOnClickListener {
+            var email=Signup_mail.text.toString()
+            var password=Signup_password.text.toString()
+
+            signup(email,password)
+        }
+
+
+
+
+
+    }
+    fun signup(email:String,password:String){
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    var intent=Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    Toast.makeText(this@SignUp,"exeption",Toast.LENGTH_LONG).show()
+
+                }
+            }
     }
 }
